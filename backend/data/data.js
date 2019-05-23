2e0c17bd17df39bcaada51fe6b255a8a90c96979
nils <nilsstre@kth.se>
2019-05-23 16:21:19 +0200

    Added filed for marking a todo as completed

--- backend/data/data.js
+++ backend/data/data.js
@@ -5,12 +5,14 @@ const todos =  [
             {
                 todo_id: 1,
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                complete_time: '2019-05-28T18:25:43.511Z'
+                complete_time: '2019-05-28T18:25:43.511Z',
+                completed: 'false'
             },
             {
                 todo_id: 2,
                 description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                complete_time: '2019-05-29T18:25:43.511Z'
+                complete_time: '2019-05-29T18:25:43.511Z',
+                completed: 'false'
             }]
     },
     {
@@ -19,12 +21,14 @@ const todos =  [
             {
                 todo_id: 1,
                 description: 'Ut enim ad minim veniam.',
-                complete_time: '2019-05-28T18:25:43.511Z'
+                complete_time: '2019-05-28T18:25:43.511Z',
+                completed: 'false'
             },
             {
                 todo_id: 2,
                 description: 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                complete_time: '2019-05-29T18:25:43.511Z'
+                complete_time: '2019-05-29T18:25:43.511Z',
+                completed: 'false'
             }]
     }
 ];
