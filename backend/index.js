46c5056fb5fbba4745854dcf4e1ed6a390da76e7
nils <nilsstre@kth.se>
2019-05-23 16:53:33 +0200

    Added API endpoints from /api/index.js

--- backend/index.js
+++ backend/index.js
@@ -4,5 +4,8 @@ const app = express()
 const PORT = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
+const api = require('./api/');
+
+app.use('/api', api)
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
