1a52d2b35ca18e793e32c5f52f8b600dfea13818
nils <nilsstre@kth.se>
2019-05-23 14:00:14 +0200

    Created file for storage of todo lists

---
+++ backend/data/todos.js
@@ -0,0 +1,32 @@
+const todos =  [
+    {
+        list_id: 1,
+        todos: [
+            {
+                todo_id: 1,
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                complete_time: '2019-05-28T18:25:43.511Z'
+            },
+            {
+                todo_id: 2,
+                description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                complete_time: '2019-05-29T18:25:43.511Z'
+            }]
+    },
+    {
+        list_id: 2,
+        todos: [
+            {
+                todo_id: 1,
+                description: 'Ut enim ad minim veniam.',
+                complete_time: '2019-05-28T18:25:43.511Z'
+            },
+            {
+                todo_id: 2,
+                description: 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                complete_time: '2019-05-29T18:25:43.511Z'
+            }]
+    }
+];
+
+module.exports = todos
