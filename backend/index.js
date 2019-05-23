04a2a729a9ca4bd5c90a35730fad875e147d1b5e
nils <nilsstre@kth.se>
2019-05-23 20:02:48 +0200

    Moved requiring bodyParser to the top since it has to called first

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const api = require('./api/');
 const app = express()
-
 const PORT = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
-const api = require('./api/');
+app.use(bodyParser.json()); // Must for some reason be called first
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', api)
 
