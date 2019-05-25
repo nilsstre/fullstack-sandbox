6b4719022dfd021d7d1117c87f26c046f0927b53
nils <nilsstre@kth.se>
2019-05-26 01:14:41 +0200

    Can now send and receive list, checkboxes for completed tasks, lists can be marked as done

--- frontend/src/todos/components/ToDoListForm.jsx
+++ frontend/src/todos/components/ToDoListForm.jsx
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button'
 import DeleteIcon from '@material-ui/icons/Delete'
 import AddIcon from '@material-ui/icons/Add'
 import Typography from '@material-ui/core/Typography'
+import Checkbox from '@material-ui/core/Checkbox'
 import { TextField } from '../../shared/FormFields'
 
 const useStyles = makeStyles({
@@ -30,70 +31,101 @@ const useStyles = makeStyles({
   }
 })
 
+let timer
+
 export const ToDoListForm = ({ toDoList, saveToDoList }) => {
   const classes = useStyles()
   const [todos, setTodos] = useState(toDoList.todos)
 
   const handleSubmit = event => {
-    event.preventDefault()
+    console.log("handleSubmit")
+
+    if (typeof event !== "undefined")
+      event.preventDefault()
+
     saveToDoList(toDoList.id, { todos })
   }
 
+  const handleComplete = (checked, index) => {
+    setTodos([ // immutable update
+      ...todos.slice(0, index),
+      {description: todos[index].description, complete_time: todos[index].complete_time, completed: checked},
+      ...todos.slice(index + 1)
+    ])
+    handleSubmit()
+  }
+
+  const handleChange = (event, index) => {
+    setTodos([ // immutable update
+      ...todos.slice(0, index),
+      {description: event.target.value, complete_time: todos[index].complete_time, completed: todos[index].completed},
+      ...todos.slice(index + 1)
+    ])
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      handleSubmit(event)
+    }, 1000)
+  }
+
+  const handleAdd = (event) => {
+    setTodos([...todos, {description: "", complete_time: Date.now(), completed: false}])
+    handleSubmit(event)
+  }
+
+
   return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography variant='headline' component='h2'>
-          {toDoList.title}
-        </Typography>
-        <form onSubmit={handleSubmit} className={classes.form}>
-          {todos.map((name, index) => (
-            <div key={index} className={classes.todoLine}>
-              <Typography className={classes.standardSpace} variant='title'>
-                {index + 1}
-              </Typography>
-              <TextField
-                label='What to do?'
-                value={name}
-                onChange={event => {
-                  setTodos([ // immutable update
-                    ...todos.slice(0, index),
-                    event.target.value,
-                    ...todos.slice(index + 1)
-                  ])
-                }}
-                className={classes.textField}
-              />
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant='headline' component='h2'>
+            {todos.filter(t => !t.completed).length === 0
+                ? <s>{toDoList.name}</s>
+                : toDoList.name}
+          </Typography>
+          <form onSubmit={handleSubmit} className={classes.form}>
+            {todos.map((todo, index) => (
+                <div key={index} className={classes.todoLine}>
+                  <Typography className={classes.standardSpace} variant='title'>
+                    {index + 1}
+                  </Typography>
+                  <TextField
+                      label='What to do?'
+                      value={todo.description}
+                      onChange={event => handleChange(event, index)}
+                      className={classes.textField}
+                  />
+                  <Checkbox
+                      checked={todo.completed}
+                      inputProps={{ 'aria-label': 'Checkbox A' } }
+                      onChange={(event, checked) => handleComplete(checked, index)}
+                  />
+                  <Button
+                      size='small'
+                      color='secondary'
+                      className={classes.standardSpace}
+                      onClick={() => {
+                        setTodos([ // immutable delete
+                          ...todos.slice(0, index),
+                          ...todos.slice(index + 1)
+                        ])
+                      }}
+                  >
+                    <DeleteIcon />
+                  </Button>
+                </div>
+            ))}
+            <CardActions>
               <Button
-                size='small'
-                color='secondary'
-                className={classes.standardSpace}
-                onClick={() => {
-                  setTodos([ // immutable delete
-                    ...todos.slice(0, index),
-                    ...todos.slice(index + 1)
-                  ])
-                }}
+                  type='button'
+                  color='primary'
+                  onClick={(event) => {
+                    handleAdd(event)
+                  }}
               >
-                <DeleteIcon />
+                Add Todo <AddIcon />
               </Button>
-            </div>
-          ))}
-          <CardActions>
-            <Button
-              type='button'
-              color='primary'
-              onClick={() => {
-                setTodos([...todos, ''])
-              }}
-            >
-              Add Todo <AddIcon />
-            </Button>
-            <Button type='submit' variant='contained' color='primary'>
-              Save
-            </Button>
-          </CardActions>
-        </form>
-      </CardContent>
-    </Card>
+            </CardActions>
+          </form>
+        </CardContent>
+      </Card>
   )
 }
