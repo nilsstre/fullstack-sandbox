e1a7587ec18e8eca7f07ecdc03e9632aa05a13f0
nils <nilsstre@kth.se>
2019-05-25 23:02:27 +0200

    Changed to fit with original data structure

--- backend/data/data.js
+++ backend/data/data.js
@@ -1,38 +1,34 @@
-const todos =  [
-    {
-        list_id: 1,
-        list_name: 'First list',
-        list_todos: [
+let todos = [{
+    '1': {
+        id: 1,
+        name: 'First list',
+        todos: [
             {
-                todo_id: 1,
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
                 complete_time: '2019-05-28T18:25:43.511Z',
-                completed: 'false'
+                completed: false
             },
             {
-                todo_id: 2,
                 description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                 complete_time: '2019-05-29T18:25:43.511Z',
-                completed: 'false'
+                completed: false
             }]
     },
-    {
-        list_id: 2,
-        list_name: 'Second list',
-        list_todos: [
+    '2': {
+        id: 2,
+        name: 'Second list',
+        todos: [
             {
-                todo_id: 1,
                 description: 'Ut enim ad minim veniam.',
                 complete_time: '2019-05-28T18:25:43.511Z',
-                completed: 'false'
+                completed: false
             },
             {
-                todo_id: 2,
                 description: 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                 complete_time: '2019-05-29T18:25:43.511Z',
-                completed: 'false'
+                completed: false
             }]
     }
-];
+}]
 
 module.exports = todos
