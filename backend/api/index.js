7b7e4a14385b613ce5feaed2718b946bc4a0c65c
nils <nilsstre@kth.se>
2019-05-26 01:12:45 +0200

    Updated answering message from server after a successful post

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const data = require('../data/data.js')
+let data = require('../data/data.js')
 
 const routs = express.Router()
 
@@ -18,13 +18,9 @@ routs.get('/todos/', (req, res) => {
  */
 routs.post('/todos/', (req, res) => {
     console.log(req.body)
-    data.map((todos, index) => {
-        if (todos.list_id == req.body.list_id) {
-            todos.list_todos = req.body.list_todos
-            return res.status(201).send({
-                message: 'the list with the list_id: ' + todos.list_id + ' was updated'
-            })
-        }
+    data = req.body
+    return res.status(201).send({
+        message: "saved lists to sever"
     })
 })
 
